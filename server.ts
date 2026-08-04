1980d40fd88d7cedfb46257fdfcb27c2a768fd24
Jothi Harshan D K <masterharshan5@gmail.com>
2026-08-04 18:46:37 +0530

    Update ESP32 factory ID and dashboard telemetry

--- server.ts
+++ server.ts
@@ -1910,7 +1910,7 @@ async function startServer() {
       console.log("ESP32 DATA RECEIVED");
       console.log(req.body);
 
-      const factoryId = "f-1785781221785";  // <-- CHANGE THIS
+      const factoryId = "f-1785846637076"; // <-- CHANGE THIS
 
       const today = new Date().toISOString().split("T")[0];
 
